ef924a494a8ce5770a93f08a5ea7abac4efc5a46
Emmycruz12 <emmycruz695@gmail.com>
2024-10-30 16:42:56 +0100

    fix issues

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -53,19 +53,21 @@ const Footer = () => {
               </p>
             </div>
 
-            <div>
-              <h1 className="text-white mb-4">CONSUMER ADVISORY</h1>
-              <div className="max-w-[463px] text-white space-y-4 ">
-                <p className="flex flex-col">
-                  These statements have not been evaluated by the Food and Drug
-                  Administration. This product is not intended to diagnose,
-                  treat, cure, or prevent any disease. This product should be
-                  used only as directed on the label. All trademarks and
-                  copyrights are property of their respective owners and not
-                  affiliated with nor do they endorse this product. Results may
-                  vary.
-                </p>
-                <p>
+            <div className="max-w-[463px]">
+              <div className="text-white">
+                <h1 className=" mb-4">CONSUMER ADVISORY</h1>
+                <div className="  space-y-4 border-r border-[#5D6544] pr-4 ">
+                  <p className="flex flex-col">
+                    These statements have not been evaluated by the Food and
+                    Drug Administration. This product is not intended to
+                    diagnose, treat, cure, or prevent any disease. This product
+                    should be used only as directed on the label. All trademarks
+                    and copyrights are property of their respective owners and
+                    not affiliated with nor do they endorse this product.
+                    Results may vary.
+                  </p>
+                </div>
+                <p className="text-white pr-4 mt-8">
                   By using our website or product, you agree to follow our terms
                   of service.
                 </p>
@@ -95,7 +97,8 @@ const Footer = () => {
                     <Image src={Linkedin} alt="Linkedin" />
                   </Link>
                 </p>
-                <p className="text-[#ABABAB]">
+
+                <p className="text-[#ABABAB]  !mt-[55px]">
                   © 2020@sleepstiq. All Rights Reserved.
                 </p>
               </div>
